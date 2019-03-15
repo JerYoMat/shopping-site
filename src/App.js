7c4e5a50de4148c004fcf407fd68a73f6eb572dd
JerYOMat <jeremy.emata@gmail.com>
2019-03-15 19:34:45 -0400

    updated Nav with click event and handler, added styling to each component and added ItemPage.  Updated both in App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import Nav from './Nav';
 import './App.css';
+import ItemPage from './ItemPage';
+import {items} from './static-data';
 
 class App extends React.Component {
-  renderContent() {
-    return(
-      <span>
-        Empty
-      </span>
-    )
+  state = { //App will keep track of which tab is currently active 
+    activeTab: 0  
+  };
+
+  handleTabChange = (index) => {  //handleTabChange is passed down into Nav along with activeTab  so it can render correctly
+    this.setState({
+      activeTab: index
+    });;
   }
+
+  renderContent() {
+    switch(this.state.activeTab) {  //switch value evaluates to 0 or 1
+      default:
+      case 0: return <ItemPage items={items}/>;
+      case 1: return <span>Cart</span>;
+    }
+  }
+
   render() {
+    let {activeTab} = this.state;
     return(
       <div className="App">
-        <Nav />
+        <Nav activeTab={activeTab} onTabChange={this.handleTabChange}/>
         <main className="App-content">
           {this.renderContent()}
         </main>
--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,19 @@
 import React from 'react';
+//The arrow functions are necessary so that the props are not evaluated until the user clicks either one.  Generally it is considered to be a bad practice to create a new function to pass into props
 
-const Nav = () => (
-  <nav className="App-nav">
+const Nav = ({ activeTab, onTabChange }) => (
+  <nav className="App-nav"> 
     <ul>
-      <li className="App-nav-item"><a>Items</a></li>
-      <li className="App-nav-item"><a>Cart</a></li>
+      <li className={`App-nav-item ${activeTab === 0 && 'selected'}`}>
+        <a onClick={()=>{onTabChange(0)}}>Items</a>
+      </li>
+      <li className={`App-nav-item ${activeTab === 1 && 'selected'}`}>
+        <a onClick={()=>{onTabChange(1)}}>Cart</a>
+      </li>
     </ul>
   </nav>
 );
+
+/*Function components and classes that extend Component are not pure. They will always re-render, even if their props haven’t changed. If a class extends PureComponent instead, though, it is pure and it will skip re-rendering when its props are unchanged. Avoiding needless re-renders is the easiest way to improve performance in a React app.
+ */
 export default Nav;
